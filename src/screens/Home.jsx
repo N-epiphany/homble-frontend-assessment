2bcba9d605a4ada6352f81c04cd1b58f0c341c60
Bhavya Srivastava <bs.epiphany14@gmail.com>
2024-05-20 07:31:24 +0530

    Update Home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -12,12 +12,7 @@ const Home = () => {
             Page 1 - Product list page and create button
           </Link>
         </li>
-        <li>
-          <Link to="http://localhost:3000/product/7">
-            Page 2 - Product detail page (this will take you to id = 7 just for
-            demo){" "}
-          </Link>
-        </li>
+        <li>Page 2 - Product detail page (Check from Product page)</li>
         <li>
           <Link to="/dashboard">Page 3 - Product dashboard</Link>
         </li>
